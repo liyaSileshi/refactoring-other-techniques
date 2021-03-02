ca018d50ac6e8b4997a96b9df11338b8a22a0281
liyaSileshi <liyasileshi@gmail.com>
2021-03-01 20:45:21 -0800

    p24

---
+++ extract_class3.py
@@ -0,0 +1,40 @@
+# by Kami Bigdely
+# Extract class
+WELL_DONE = 3000
+MEDIUM = 2500
+COOKED_CONSTANT = 0.05
+
+
+class Cook:
+
+  def __init__(self, time, temperature, pressure, desired_state):
+    self.time = time
+    self.temperature = temperature
+    self.pressure = pressure
+    self.desired_state = desired_state
+
+  def is_cookeding_criteria_satisfied(self):
+    return self.is_well_done() or \
+           self.is_medium()
+
+  def is_well_done(self):    
+    return self.desired_state == 'well-done' and  \
+           self.get_cooking_progress() >= WELL_DONE
+
+  def is_medium(self):
+    return self.desired_state == 'medium' and  \
+           self.get_cooking_progress() >= MEDIUM
+
+  def get_cooking_progress(self):
+    return self.time * self.temperature * self.pressure * COOKED_CONSTANT
+
+if __name__ == "__main__":
+  time = 30 # [min]
+  temp = 103 # [celcius]
+  pressure = 20 # [psi]
+  desired_state = 'well-done'
+  cook = Cook(time, temp, pressure, desired_state)
+  if cook.is_cookeding_criteria_satisfied():
+      print('cooking is done.')
+  else:
+      print('ongoing cooking.')
